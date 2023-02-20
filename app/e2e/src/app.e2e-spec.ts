0c90fa464c0398903ee1812056d5ba0ab5d8e0b7
Thomas Bergmann <thomas@hatoka.de>
2023-02-20 19:12:36 +0100

    feat: build angular app at ci (#11) * declare location of package.lock * execute npm at app folder * update docker image to use node 18

--- app/e2e/src/app.e2e-spec.ts
+++ app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('cloud-tenant app is running!');
+    expect(await page.getTitleText()).toEqual('poker-app app is running!');
   });
 
   afterEach(async () => {
--- app/karma.conf.js
+++ app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cloud-tenant'),
+      dir: require('path').join(__dirname, './coverage/poker-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
