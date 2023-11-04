779d8a172decc1c7ee5a477604f1286ccc80bdda
Thomas Bergmann <thomas@hatoka.de>
2023-11-04 11:22:05 +0100

    chore: replace javax.validation with jakarta.validation

--- player/src/main/java/de/hatoka/poker/player/capi/remote/BotInfoRO.java
+++ player/src/main/java/de/hatoka/poker/player/capi/remote/BotInfoRO.java
@@ -1,6 +1,6 @@
 package de.hatoka.poker.player.capi.remote;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
--- poker/src/main/java/de/hatoka/poker/remote/oauth/OAuthRefreshRO.java
+++ poker/src/main/java/de/hatoka/poker/remote/oauth/OAuthRefreshRO.java
@@ -1,6 +1,6 @@
 package de.hatoka.poker.remote.oauth;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
